55e1038e46e65753a125f5d78eca438f06ae9b55
Ivan Besarab <IvanVBesarab@gmail.com>
2020-08-12 12:52:13 +0300

    Add task description fixes, update .gitignore

--- Assignment2/Assignment2/ViewController.swift
+++ Assignment2/Assignment2/ViewController.swift
@@ -10,17 +10,17 @@ import UIKit
 
 class ViewController: RootViewController {
 
-    //    Задача 0. Вернуть наибольшее из двух чисел
+    //    Задача 0. Вывести наибольшее из двух чисел
     func findGreatestNumber(numberA: Int, numberB: Int) {
         print("Greatest number is 9")
     }
 
-    //    Задача 1. Вернуть квадрат и куб введенного числа
+    //    Задача 1. Вывести квадрат и куб введенного числа
     func findSquareAndQubeOf(number: Int) {
         fatalError()
     }
 
-    //    Задача 2. Вывести на экран все числа до заданного и в обратном порядке до 0
+    //    Задача 2. Вывести все числа до заданного и в обратном порядке до 0
     func numbersUpToNAndBackToZero(with number: Int) {
         fatalError()
     }
@@ -30,7 +30,7 @@ class ViewController: RootViewController {
         fatalError()
     }
 
-    //    Задача 4 Проверить, является ли заданное число совершенным и найти их
+    //    Задача 4 Проверить, является ли заданное число совершенным
     func isNumberPerfect(_ number: Int) {
         fatalError()
     }
