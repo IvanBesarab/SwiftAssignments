48d29c34e2c1de9320e4dd3ee3f1446caf338970
Ivan Besarab <IvanVBesarab@gmail.com>
2020-08-17 19:48:09 +0300

    Add Exam2 view controller dummy

---
+++ Exam2/Exam2/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Exam2
+//
+//  Created by Ivan Besarab on 8/17/20.
+//  Copyright © 2020 Ivan Besarab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: RootViewController {
+    //    1. Записать метод которий виведет в консоль сообщение С приветсвием "Привет” (без аргументов) - 5мин -макс
+
+    func sayHello() {
+        fatalError()
+    }
+
+    //    2. Записать метод которий будет выводить в консоль сообщение какой день недели относительно числа которое попадает в метод как аргумент (1 аргумент) 10 мин -макс
+
+    func showDayOfWeek(dayNumber: Int) {
+        fatalError()
+    }
+
+    //    3. Записать метод которий выводит в консоль суму двух чисел  (2 аргумента) 10 мин -макс
+
+    func sum(numberA: Int, numberB: Int) {
+        fatalError()
+    }
+
+    //    4. Записать метод которий будет умножать задание число на 30 прибавлять 5 и возвращать результат вичислений, єтот результат нужно использовать в методе viewDidLoad для вивода в консоль  (1 аргумент + return) 15мин -макс (2 * 30 + 5 )
+    //    print(calc(number: 2)) // in viewDidLoad
+
+    func calc(number: Int) -> Int {
+        fatalError()
+    }
+
+    //    5. Записать метод которий виведет в консоль сообщение С приветсвием "Привет” заданное количество раз (1 аргумент) - 5мин -макс
+
+    func sayHello(repeatCount: Int) {
+        fatalError()
+    }
+
+    //    6. Написать метод который будет показывать число Фибоначчи например fib(num:6) должно вернуть число 8 (1 аргумент + return)
+    func fibanachi(number: Int) -> Int {
+        fatalError()
+    }
+
+}
+
