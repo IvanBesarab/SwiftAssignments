e17136b546708fd19e470c1518f0b924e9e50878
Ivan Besarab <IvanVBesarab@gmail.com>
2020-08-20 13:27:59 +0300

    Add String helpers

---
+++ Assignment3/Assignment3/String+Helpers.swift
@@ -0,0 +1,31 @@
+//
+//  String+Helpers.swift
+//  Assignment3
+//
+//  Created by Ivan Besarab on 8/20/20.
+//  Copyright © 2020 RockSoft. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func index(from: Int) -> Index {
+        return self.index(startIndex, offsetBy: from)
+    }
+
+    func substring(from: Int) -> String {
+        let fromIndex = index(from: from)
+        return String(self[fromIndex...])
+    }
+
+    func substring(to: Int) -> String {
+        let toIndex = index(from: to)
+        return String(self[..<toIndex])
+    }
+
+    func substring(with r: Range<Int>) -> String {
+        let startIndex = index(from: r.lowerBound)
+        let endIndex = index(from: r.upperBound)
+        return String(self[startIndex..<endIndex])
+    }
+}
