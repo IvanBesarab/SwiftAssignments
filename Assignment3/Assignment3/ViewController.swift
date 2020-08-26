ec960aa02793d2a0ac603350d8f18d8fb0ee4b50
Viktor Kyrychenko <viktor.kyrychenko@me.com>
2020-08-27 00:15:54 +0300

    add tests 1-6

--- Assignment3/Assignment3/ViewController.swift
+++ Assignment3/Assignment3/ViewController.swift
@@ -10,53 +10,100 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    //    Задача 1. Создать строку с своим именем, вывести количество символов содержащихся в ней.
-
+ //    Задача 1. Создать строку с своим именем, вывести количество символов содержащихся в ней.
+    
     func countStringCharacters(name: String) -> Int {
         return name.count
     }
-
+    
     //    Задача 2. Создать строку с своим отчеством проверить его на окончание “ич/на”
-
+    
     func checkPatronomic(name: String) -> Bool {
-            if name.hasSuffix("ich") {
+        if name.hasSuffix("ich") {
             return true
         }
-            if name.hasSuffix("vna") {
-                return true
-            }
-            else {
-                return false
-            }
+        if name.hasSuffix("vna") {
+            return true
+        }
+        else {
+            return false
+        }
     }
-
+    
     //    Задача 3. Cоздать строку, где слитно написано Ваши ИмяФамилия “IvanVasilevich"
-
+    
     func divideCredentials(fullName: String) -> String {
         fatalError()
     }
-
+    
     //    Задача 4. Вывести строку зеркально Ось → ьсО не используя reverse
-
+    
     func mirrorString(input: String) -> String {
         var str = ""
-         for character in input {
+        for character in input {
             str = "\(character)" + str
-         }
-         return str
         }
-
+        return str
+    }
+    
     //    Задача 5. Добавить запятые в строку как их расставляет калькулятор
-
+    
     func formatNumber(input: String, separator: String) -> String {
-        fatalError()
+        var newText : String = ""
+        for character in input {
+            newText.insert(character, at: newText.startIndex)
+        }
+        var result : String = ""
+        for (index , number) in newText.enumerated() {
+            if index != 0 && index % 3 == 0 {
+                result.append(separator)
+            }
+            result.append(number)
+        }
+        var finalResult : String = ""
+        for i in result {
+            finalResult.insert(i, at: finalResult.startIndex)
+        }
+        return finalResult
     }
-
+    
     //    Задача 6. Проверить пароль на надежность от 1 до 5
-
+    
     func checkPassword(password: String) -> Int {
-        fatalError()
+        var markNumbers = 0
+        var markLetter = 0
+        var markUppercased = 0
+        var markSymbol = 0
+        var totalScore = 0
+        for character in password {
+            if character.isNumber {
+                markNumbers += 1
+            }
+            if character.isLetter {
+                markLetter += 1
+            }
+            if character.isUppercase {
+                markUppercased += 1
+            }
+            if character.isPunctuation {
+                markSymbol += 2
+            }
+        }
+        if markNumbers >= 1 {
+            totalScore += 1
+        }
+        if markLetter >= 1 {
+            totalScore += 1
+        }
+        if markUppercased >= 1 {
+            totalScore += 1
+        }
+        if markSymbol >= 1 {
+            totalScore += 2
+        }
+        return totalScore
     }
+
 
     //    Задача 7. Сортировка массива не встроенным методом по возрастанию + удалить дубликаты
 
