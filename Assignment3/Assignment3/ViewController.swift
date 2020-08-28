f8f0d70d04799be537b8b2f0c26b1b972c55132e
Artem Manakov <artem.manakov0@gmail.com>
2020-08-28 21:36:24 +0300

    Assignment3-test2 and assignment3-test3, assignment3-test5 and assignment3-test6

--- Assignment3/Assignment3/ViewController.swift
+++ Assignment3/Assignment3/ViewController.swift
@@ -9,75 +9,175 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     //    Задача 1. Создать строку с своим именем, вывести количество символов содержащихся в ней.
-
+    
     func countStringCharacters(name: String) -> Int {
         print("Меня зовут \(name)")
         return name.count
         
     }
-
+    
     //    Задача 2. Создать строку с своим отчеством проверить его на окончание “ич/на”
-
+    
     func checkPatronomic(name: String) -> Bool {
-        fatalError()
+        if name.hasSuffix("ich") {
+            return true
+        } else if name.hasSuffix("na") {
+            return true
+        } else {
+            return false
+        }
     }
-
+    
     //    Задача 3. Cоздать строку, где слитно написано Ваши ИмяФамилия “IvanVasilevich"
-
+    
     func divideCredentials(fullName: String) -> String {
-        fatalError()
+        var indexCharacter = 0
+        var arrayName = [Character]()
+        for _ in 0..<fullName.count {
+            let index = fullName.index(fullName.startIndex, offsetBy: indexCharacter)
+            indexCharacter += 1
+            arrayName += [fullName[index]]
+        }
+        var indexLetter = 1 // пропускаю 0 потому что "А" большая
+        var letter = arrayName[indexLetter]
+        while letter != Character(letter.uppercased()) {
+            indexLetter += 1
+            letter = arrayName[indexLetter]
+        }
+        let name = fullName.substring(to: indexLetter)
+        let surname = fullName.substring(from: indexLetter)
+        let spaceBetweenWords = "\(name)"+" "+"\(surname)"
+        return spaceBetweenWords
     }
-
+    
+    
     //    Задача 4. Вывести строку зеркально Ось → ьсО не используя reverse
-
+    
     func mirrorString(input: String) -> String {
-        print(input)
         var word = input
         var mirrorWord = String()
         for _ in 0..<word.count {
-            var endLetter = word.remove(at: word.index(before: word.endIndex))
+            let endLetter = word.remove(at: word.index(before: word.endIndex))
             mirrorWord += String(endLetter)
         }
         return mirrorWord
     }
-
+    
     //    Задача 5. Добавить запятые в строку как их расставляет калькулятор
-
+    
     func formatNumber(input: String, separator: String) -> String {
-        fatalError()
+        var figures = input
+        var mirrorFigures = String()
+        for _ in 1...figures.count {
+            let endLetter = figures.remove(at: figures.index(before: figures.endIndex))
+            mirrorFigures += String(endLetter)
+        }
+        var indexMirrorFigures = 0
+        var arrayMirrorFigures = [Character]()
+        while indexMirrorFigures != mirrorFigures.count {
+            let index = mirrorFigures.index(mirrorFigures.startIndex, offsetBy: indexMirrorFigures)
+            arrayMirrorFigures += [mirrorFigures[index]]
+            indexMirrorFigures += 1
+        }
+        var howManyFigures = 4
+        var indexArrayMirrorFigures = 3
+        let HowManyCharacterAreInArray = arrayMirrorFigures // без этой строки не работает код
+        while howManyFigures <= HowManyCharacterAreInArray.count {
+            arrayMirrorFigures.insert(contentsOf: separator, at: indexArrayMirrorFigures)
+            howManyFigures += 3
+            indexArrayMirrorFigures += 4
+        }
+        var returnNormalFigures = String() // мы возвращаем порядок цифр, который был до зеркального отображения
+        for _ in 0..<arrayMirrorFigures.count {
+            let endLetterArray = arrayMirrorFigures.removeLast()
+            returnNormalFigures += String(endLetterArray)
+        }
+        return String(returnNormalFigures)
     }
-
+    
     //    Задача 6. Проверить пароль на надежность от 1 до 5
-
+    
     func checkPassword(password: String) -> Int {
-        fatalError()
+        let arraySmallLetters = ["a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
+        let arrayFigures = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+        let arrayBigLetters = ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"]
+        let arraySigns = ["!", "@", "#", "$", "%", "&", "^", "*", "-", "_", "=", "+", "`", "~",]
+        var arrayPassword = [Character]()
+        var indexNumber = 0
+        while indexNumber != password.count {
+            let index = password.index(password.startIndex, offsetBy: indexNumber)
+            arrayPassword += [password[index]]
+            indexNumber += 1
+        }
+        var pointForSmallLetter = 0
+        var pointForFigures = 0
+        var pointForBigLetters = 0
+        var pointForSigns = 0
+        var indexArrayPassword = 0
+        while indexArrayPassword != arrayPassword.count {
+            if arraySmallLetters.contains(String(arrayPassword[indexArrayPassword])) {
+                pointForSmallLetter += 1
+            }
+            if arrayFigures.contains(String(arrayPassword[indexArrayPassword])) {
+                pointForFigures += 1
+            }
+            if arrayBigLetters.contains(String(arrayPassword[indexArrayPassword])) {
+                pointForBigLetters += 1
+            }
+            if arraySigns.contains(String(arrayPassword[indexArrayPassword])) {
+                pointForSigns += 1
+            }
+            indexArrayPassword += 1
+        }
+        if pointForSmallLetter > 0 {
+            pointForSmallLetter = 1
+        } else {
+            pointForSmallLetter = 0
+        }
+        if pointForFigures > 0 {
+            pointForFigures = 1
+        } else {
+            pointForFigures = 0
+        }
+        if pointForBigLetters > 0  {
+            pointForBigLetters = 1
+        } else {
+            pointForBigLetters = 0
+        }
+        if pointForSigns > 0 {
+            pointForSigns = 2
+        } else {
+            pointForSigns = 0
+        }
+        let sum = pointForSmallLetter + pointForFigures + pointForBigLetters + pointForSigns
+        return  sum
     }
-
+    
+    
     //    Задача 7. Сортировка массива не встроенным методом по возрастанию + удалить дубликаты
-
+    
     func sortArrayAndRemoveDuplicates(input: [Int]) -> [Int] {
         fatalError()
     }
-
+    
     //    Задача 8. Написать метод, который будет переводить строку в транслит.
-
+    
     func translite(input: String) -> String {
         fatalError()
     }
-
+    
     //    Задача 9. Сделать выборку из массива строк в которых содержится указанная строка
-
+    
     func sortArray(input: [String], searchRequest: String) -> [String] {
         fatalError()
     }
-
+    
     //    Задача 10. Убрать запрещенные слова из текста
-
+    
     func filterText(input: String, bannedWords: [String]) -> String {
         fatalError()
     }
-
+    
 }
-
