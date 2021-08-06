2edf7ee9ea2c0c30f63b83d0ec724d03798c9e00
IvanPavlenk0 <ivanpavlenko.025@gmail.com>
2021-08-06 21:51:25 +0300

    Optimized assignment 2

--- Assignment2/Assignment2/ViewController.swift
+++ Assignment2/Assignment2/ViewController.swift
@@ -17,11 +17,16 @@ class ViewController: RootViewController {
 
     //    Задача 1. Вывести квадрат и куб введенного числа
     func findSquareAndQubeOf(number: Int) {
-        let a = number * number
-        print("""
-square of \(number) = \(a)
-qube of \(number) = \(a * number)
-""")
+        func square(_ number: Int) -> Int {
+            let result = number * number
+            return result
+        }
+        func qube(_ number: Int) -> Int {
+            let result = square(number) * number
+            return result
+        }
+        print("square of \(number) = \(square(number))")
+        print("qube of \(number) = \(qube(number))")
     }
 
     //    Задача 2. Вывести все числа до заданного и в обратном порядке до 0
@@ -41,25 +46,12 @@ qube of \(number) = \(a * number)
 
     //    Задача 3. Подсчитать общее число делителей числа и вывести их
     func findDivisors(of number: Int) {
-        var divisorsCount = 0
-        for i in 1..<number {
-            if number % i == 0 {
-                print(i)
-                divisorsCount += 1
-            }
-        }
-        print("number \(number) has \(divisorsCount) divisors")
+        print("number \(number) has \(numberDivisors(number: number, needPrint: true)) divisors")
     }
 
     //    Задача 4 Проверить, является ли заданное число совершенным
     func isNumberPerfect(_ number: Int) {
-        var divisorsCount = 0
-        for i in 1..<number {
-            if number % i == 0 {
-                divisorsCount += i
-            }
-        }
-        if number == divisorsCount {
+        if number == numberDivisors(number: number, needPrint: false) {
             print("number \(number) is perfect")
         }
         else {
@@ -70,26 +62,40 @@ qube of \(number) = \(a * number)
 	//    Задача 5 Развернуть число
 	func mirror(number: Int) {
         var num = number
-        var lengthOfNumber = 1
-        var multiplier = 10
-        while multiplier < num {
-            lengthOfNumber += 1
-            multiplier *= 10
-        }
+        var multiplier = 1
         var reNumber = 0
         var reNumberCounter = 0
-        while multiplier > num {
+        while multiplier < number {
             if num % 10 != 0 {
                 num -= 1
                 reNumberCounter += 1
             } else {
                 num /= 10
-                multiplier /= 10
-                reNumber = reNumberCounter * multiplier + reNumber
+                reNumber *= 10
+                reNumber += reNumberCounter
+                multiplier *= 10
                 reNumberCounter = 0
             }
         }
         print ("Mirrored number is \(reNumber)")
+    }
+    
+    func numberDivisors(number: Int, needPrint: Bool) -> Int { // for task 3 & 4
+        var divisorsCount = 0
+        var divisorsSumm = 0
+        for i in 1..<number {
+            if number % i == 0 {
+                if needPrint == true {
+                    print(i)
+                }
+                divisorsCount += 1
+                divisorsSumm += i
+            }
+        }
+        if needPrint == true {
+            return divisorsCount
+        }
+        return divisorsSumm
     }
 
 }
